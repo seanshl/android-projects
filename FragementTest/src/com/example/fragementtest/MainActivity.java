efb8c42c834025e96d8bee2e0f1d25b008e679e3
Shiyao Liu <liushiyao2014@gmail.com>
2016-08-23 16:53:08 -0500

    Add dynamic fragment by FragmentManager

--- FragementTest/src/com/example/fragementtest/MainActivity.java
+++ FragementTest/src/com/example/fragementtest/MainActivity.java
@@ -1,14 +1,39 @@
 package com.example.fragementtest;
 
 import android.app.Activity;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements OnClickListener {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button button = (Button) this.findViewById(R.id.button);
+		button.setOnClickListener(this);
+	}
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		switch (v.getId()) {
+		case R.id.button:
+			AnotherRightFragment fragment = new AnotherRightFragment();
+			
+			FragmentManager fragmentManager = this.getFragmentManager();
+			FragmentTransaction transaction = fragmentManager.beginTransaction();
+			transaction.replace(R.id.right_layout, fragment);
+			transaction.commit();
+			
+			break;
+		default: break;
+		}
 	}
 
 }
