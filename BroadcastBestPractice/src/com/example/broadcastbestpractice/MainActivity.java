4d89f6473bd4526f852ddf29b367a88250eaf6b0
Shiyao Liu <liushiyao2014@gmail.com>
2016-08-31 17:52:02 -0500

    Add functionalities

--- BroadcastBestPractice/src/com/example/broadcastbestpractice/MainActivity.java
+++ BroadcastBestPractice/src/com/example/broadcastbestpractice/MainActivity.java
@@ -1,15 +1,27 @@
 package com.example.broadcastbestpractice;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends BaseActivity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		Button forceOffline = (Button) this.findViewById(R.id.force_offline);
+		forceOffline.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent("com.example.broadcastbestpractice.FORCE_OFFLINE");
+				MainActivity.this.sendBroadcast(intent);
+			}
+			
+		});
 	}
 }
