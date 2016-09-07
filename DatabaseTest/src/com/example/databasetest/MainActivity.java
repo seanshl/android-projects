011be7ea6bc9ef5ed40318968947da7f0e28a8b1
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-07 17:21:33 -0500

    Add MyDBOpenHelper

--- DatabaseTest/src/com/example/databasetest/MainActivity.java
+++ DatabaseTest/src/com/example/databasetest/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.databasetest;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
-
+	
+	private MyDatabaseHelper dbHelper;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		dbHelper = new MyDatabaseHelper(this, "BookStore.db", null, 1);
+		Button createDatabase = (Button) this.findViewById(R.id.create_database);
+		createDatabase.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				dbHelper.getWritableDatabase();
+			}
+			
+		});
 	}
 
 }
