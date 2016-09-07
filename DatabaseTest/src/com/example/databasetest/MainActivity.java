3f2074c54c382ec35671593c65953a7db74ab35b
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-07 17:52:49 -0500

    Add query button

--- DatabaseTest/src/com/example/databasetest/MainActivity.java
+++ DatabaseTest/src/com/example/databasetest/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.databasetest;
 
 import android.app.Activity;
 import android.content.ContentValues;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -56,11 +58,39 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				SQLiteDatabase db = dbHelper.getWritableDatabase();
-				
+
 				ContentValues values = new ContentValues();
 				values.put("price", 10.99);
-				
+
 				db.update("Book", values, "name = ?", new String[] {"The Da Vinci Code"});
+			}
+		});
+
+		Button queryButton = (Button) findViewById(R.id.query_data);
+		queryButton.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				SQLiteDatabase db = dbHelper.getWritableDatabase();
+
+				Cursor cursor = db.query("Book", null, null, null, null, null, null);
+
+				if (cursor.moveToFirst()) {
+					do {
+						String name = cursor.getString(cursor.
+								getColumnIndex("name"));
+						String author = cursor.getString(cursor.
+								getColumnIndex("author"));
+						int pages = cursor.getInt(cursor.getColumnIndex
+								("pages"));
+						double price = cursor.getDouble(cursor.
+								getColumnIndex("price"));
+						Log.d("MainActivity", "book name is " + name);
+						Log.d("MainActivity", "book author is " + author);
+						Log.d("MainActivity", "book pages is " + pages);
+						Log.d("MainActivity", "book price is " + price);
+					} while (cursor.moveToNext());
+				}
+
 			}
 		});
 	}
