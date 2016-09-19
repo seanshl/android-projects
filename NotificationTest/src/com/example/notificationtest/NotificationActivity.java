b82a124f890b1edfc5d98189a6c12788549538a1
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-19 17:29:05 -0500

    Add NotificationActivity

---
+++ NotificationTest/src/com/example/notificationtest/NotificationActivity.java
@@ -0,0 +1,13 @@
+package com.example.notificationtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class NotificationActivity extends Activity {
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.setContentView(R.layout.notification_layout);
+	}
+}
