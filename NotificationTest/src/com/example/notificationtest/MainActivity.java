49e561351dcc5cac11a47acb996c41f9e56d08d6
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-19 17:35:42 -0500

    Impelemnt another activity for notification

--- NotificationTest/src/com/example/notificationtest/MainActivity.java
+++ NotificationTest/src/com/example/notificationtest/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.notificationtest;
 
 import android.app.Notification;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -27,7 +29,12 @@ public class MainActivity extends ActionBarActivity implements OnClickListener {
 		case R.id.send_notice:
 			NotificationManager manager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
 			Notification.Builder builder = new Notification.Builder(this);
+			
+			Intent intent = new Intent(this, NotificationActivity.class);
+			PendingIntent pi = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+			
 			builder.setSmallIcon(R.drawable.ic_launcher)
+					.setContentIntent(pi)
 					.setTicker("This is ticker text")
 					.setWhen(System.currentTimeMillis())
 					.setContentTitle("This is content title")
--- NotificationTest/src/com/example/notificationtest/NotificationActivity.java
+++ NotificationTest/src/com/example/notificationtest/NotificationActivity.java
@@ -1,6 +1,8 @@
 package com.example.notificationtest;
 
 import android.app.Activity;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 
 public class NotificationActivity extends Activity {
@@ -9,5 +11,9 @@ public class NotificationActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.notification_layout);
+		
+		NotificationManager manager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+		
+		manager.cancel(1);
 	}
 }
