6788dd91c7d17914fb1e90baa871e5b5b53b922b
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-19 17:20:53 -0500

    Using new notificationbuilder to build the notification

--- NotificationTest/src/com/example/notificationtest/MainActivity.java
+++ NotificationTest/src/com/example/notificationtest/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.notificationtest;
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
@@ -9,17 +12,32 @@ import android.widget.Button;
 public class MainActivity extends ActionBarActivity implements OnClickListener {
 
 	private Button sendNotice;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.sendNotice = (Button)this.findViewById(R.id.send_notice);
+		this.sendNotice.setOnClickListener(this);
 	}
 
 	@Override
-	public void onClick(View arg0) {
-		// TODO Auto-generated method stub
-		
+	public void onClick(View v) {
+		switch (v.getId()) {
+		case R.id.send_notice:
+			NotificationManager manager = (NotificationManager) this.getSystemService(Context.NOTIFICATION_SERVICE);
+			Notification.Builder builder = new Notification.Builder(this);
+			builder.setSmallIcon(R.drawable.ic_launcher)
+					.setTicker("This is ticker text")
+					.setWhen(System.currentTimeMillis())
+					.setContentTitle("This is content title")
+					.setContentText("This is content text");
+			Notification notification = builder.build();
+			manager.notify(1, notification);
+			break;
+		default:
+			break;
+
+		}
 	}
 }
