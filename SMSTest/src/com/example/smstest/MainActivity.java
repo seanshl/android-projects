9a1f2d41245886a4d2f54ee7c23dc0ff14bc105a
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-21 17:48:03 -0500

    Add Messge Receiver

--- SMSTest/src/com/example/smstest/MainActivity.java
+++ SMSTest/src/com/example/smstest/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.smstest;
 
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.telephony.SmsMessage;
 import android.widget.TextView;
 
 public class MainActivity extends ActionBarActivity {
@@ -17,5 +21,27 @@ public class MainActivity extends ActionBarActivity {
 		this.sender = (TextView)this.findViewById(R.id.sender);
 		this.content = (TextView)this.findViewById(R.id.content);
 	}
+	
+	protected class MessageReceiver extends BroadcastReceiver {
 
+		@Override
+		public void onReceive(Context context, Intent intent) {
+			Bundle bundle = intent.getExtras();
+			Object[] pdus = (Object[]) bundle.get("pdus");
+			SmsMessage[] messages = new SmsMessage[pdus.length];
+			
+			for (int i = 0; i < messages.length; i++) {
+				messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+			}
+			String address = messages[0].getOriginatingAddress();
+			
+			String fullMessage = "";
+			for (SmsMessage message : messages) {
+				fullMessage += message.getMessageBody();
+			}
+			sender.setText(address);
+			content.setText(fullMessage);
+		}
+		
+	}
 }
