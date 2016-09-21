a261bed561886cad04054d8f9c3c94701e3b9efb
Shiyao Liu <liushiyao2014@gmail.com>
2016-09-21 17:51:57 -0500

    Add intentfilter and receiver

--- SMSTest/src/com/example/smstest/MainActivity.java
+++ SMSTest/src/com/example/smstest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.smstest;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.telephony.SmsMessage;
@@ -14,12 +15,26 @@ public class MainActivity extends ActionBarActivity {
 	
 	private TextView content;
 	
+	private IntentFilter receiveFilter;
+	
+	private MessageReceiver messageReceiver;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		this.sender = (TextView)this.findViewById(R.id.sender);
 		this.content = (TextView)this.findViewById(R.id.content);
+		this.receiveFilter = new IntentFilter();
+		receiveFilter.addAction("android.provider.Telephony.SMS_RECEIVED");
+		this.messageReceiver = new MessageReceiver();
+		this.registerReceiver(messageReceiver, receiveFilter);
+	}
+	
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		this.unregisterReceiver(messageReceiver);
 	}
 	
 	protected class MessageReceiver extends BroadcastReceiver {
